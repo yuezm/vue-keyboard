0e49ad2dd1e95ef86ef3e0be4d634cb43a1a36f3
zhiming.yue <zhiming.yue@nx-engine.com>
2018-11-06 17:51:31 +0800

    fix: 修正部分Bug，增加ci

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import './assets/style/index.less';
 import keyboardInput from './components/KeyboardInput.vue';
+import Keyboard from './components/keyboardInput/Keyboard';
+import NumberList from './components/keyboardInput/NumberList';
 
 
 const keyboardComponent = {
   install(Vue) {
     Vue.component('keyboardInput', keyboardInput);
+    Vue.component('Keyboard', Keyboard);
+    Vue.component('NumberList', NumberList);
   },
 };
 
